f973bd163099805fcb90c717a322dde35fdfb13b
Rupesh kumar <rupesh18123143@gmail.com>
2025-07-14 19:37:16 +0530

    adding template

---
+++ client/src/components/ModernTemplate.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function ModernTemplate({ data }) {
+  return (
+    <div className="border p-3">
+      <h2>{data.name}</h2>
+      <p>{data.email} | {data.phone}</p>
+      {/* Render education, experience, skills */}
+      <h4>Education</h4>
+      {/* ... */}
+      <h4>Experience</h4>
+      {/* ... */}
+      <h4>Skills</h4>
+      {/* ... */}
+    </div>
+  );
+}
+export default ModernTemplate
