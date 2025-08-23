d885b9c28be0029656218d85a98f2538a84c9885
Rupesh kumar <rupesh18123143@gmail.com>
2025-08-23 21:39:34 +0530

    change in port number

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopol
 // Add auth routes
 app.use("/api/auth", authRoutes);
 app.use("/api/resumes", resumeRoutes);
-
+//change in port
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
